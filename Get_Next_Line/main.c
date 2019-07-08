7fe74ea85474fdd2e822c6c171dac871140822fb
Elisaveta Golubotskaya <40807065+EmmanuelSchoen@users.noreply.github.com>
2019-07-08 09:00:38 +0300

    Update main.c

--- Get_Next_Line/main.c
+++ Get_Next_Line/main.c
@@ -10,16 +10,17 @@ int		main()
     char	c;
     FILE	*f;
     
-	filename = "TEST.txt";
-    fd = open(filename, O_RDONLY);
-    printf ("%s", "\n\n\nFile was opened\n\n\n\n Your result is:\n\n");
-    printf("%d", get_next_line(fd, &line));
-    close (fd);
-    fd++;
-    printf("%s", "\n\n\n\n\nAnd that's how it should looks like: \n\n\n\n");
+	/*filename = "WAP.txt";
+	printf("%s", "\n\n\n\n\nAnd that's how it should looks like: \n\n\n\n");
     f=fopen(filename,"rt");
     while((c=fgetc(f))!=EOF)
         printf("%c",c);
-    fclose(f);
+    fclose(f); 
+	*/
+    fd = open(filename, O_RDONLY);
+    printf ("%s", "\n\n\nFile was opened\n\n\n\nYour result is:\n\n");
+    printf("%d", get_next_line(fd, &line));
+    close (fd);
+    fd++; 
     return (0);
 }
